9fb41ceb3de580083fcc2121fd70435d7ac1e716
Joao Oliveira <43117830+j-000@users.noreply.github.com>
2023-07-23 21:59:22 +0100

    New prop firstElementKey

--- models/feed.js
+++ models/feed.js
@@ -1,14 +1,18 @@
 class Feed {
-  constructor(name, url, type, orgid){
+  constructor(name, url, type, orgid, firstElementKey){
     this.name = name;
     this.url = url;
     this.type = type;
     this.orgid = orgid;
+    this.firstElementKey = firstElementKey
+    // importedjobs
+    // history
   }
 
   toJSON(){
     return {...this };
   }
+
 }
 
 
