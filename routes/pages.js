cd9af4b3e700fe7ada188de7eca7a6199b388f20
Joao Oliveira <43117830+j-000@users.noreply.github.com>
2023-07-23 14:12:39 +0100

    bunch of routes

--- routes/pages.js
+++ routes/pages.js
@@ -1,10 +1,22 @@
 const express = require('express');
-const { jwtrequired } = require('../middlewares/authentication');
+const { AuthRequired } = require('../middlewares/authentication');
 const pageRoutes = express.Router()
 
 
+// pageRoutes.route('/')
+
+
 pageRoutes.get('/login', (req, res) => {
+    if (req.cookies.token) return res.redirect('/')
     res.render('login.html');
+})
+
+
+pageRoutes.get('/logout', (req, res) => {
+    if (req.cookies.token) {
+        res.clearCookie('token');
+    }
+    return res.redirect('/login');
 })
 
 
@@ -13,12 +25,23 @@ pageRoutes.get('/register',(req, res) => {
 })
 
 
-pageRoutes.get('/', jwtrequired, (req, res) => {
-    res.render('index.html', )
+pageRoutes.get('/forgot-password',(req, res) => {
+    res.render('forgot-password.html');
 })
 
-pageRoutes.get('/feeds', jwtrequired, (req, res) => {
+
+pageRoutes.get('/', AuthRequired, (req, res) => {
+    res.render('dashboard.html')
+})
+
+
+pageRoutes.get('/feeds', AuthRequired, (req, res) => {
     res.render('feeds.html');
+})
+
+
+pageRoutes.get('/feeds/:feedid', AuthRequired, (req, res) => {
+    res.render('feeds_details.html', {feedid: req.params.feedid});
 })
 
 
