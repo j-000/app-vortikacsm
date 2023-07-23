27e455b2e5b624ce67daf8db95b9fad931ebaf69
Joao Oliveira <43117830+j-000@users.noreply.github.com>
2023-07-23 13:18:37 +0100

    Created auth middleware to validate JWT tokens in /api/ calls (header) and /<page> views (cookie)

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,71 +1,65 @@
 const jwt = require('jsonwebtoken');
-
-
 const SECRET_KEY = 'CHANGEME';
 
 
-// This functions ensures API calls use JWT token
-function jwtrequired(req, res, next) {
+function AuthRequired(req, res, next) {
   // /API/ calls should always send the token in the Authorization header. Even if the cookie is set.
+  
   // Use .originalUrl and not .url to compare full path.
   if(req.originalUrl.includes('/api/')){
     const authHeader = req.headers.authorization;
+
     // Check header is set and contains "Bearer".
     if (authHeader && authHeader.startsWith('Bearer ')) {
+
       // Check token is present
       if (authHeader.split(' ').length > 1) {
+
         const token = authHeader.split(' ')[1];
-        // Validate token
-        jwt.verify(req.cookies.token, SECRET_KEY, (err, decoded) => {
+        jwt.verify(token, SECRET_KEY, (err, decoded) => {
           if(err){
             res.json({error: 'Invalid token.'});
             return
           } else {
             // Set decoded user to request to use in view controller.
+            // controllers will have access to user.token, user.orgid and user.permissions (set in contollers.authentication.js)
             req.user = decoded;
             next();
-            return
           }
         })
       } else {
         // Invalid token format
-        res.status(401).json({
-          error: 'Invalid token format.'
-        });
-        return;
+        res.status(401).json({ error: 'Invalid token format.' });
       }
     } else {
       // No cookie/auth header
-      res.status(401).json({
-        error: 'No token provided.'
-      })
-      return;
+      res.status(401).json({ error: 'No token provided.' });
     }
   } else {
     // Check token is set in cookie
     if (req.cookies.token) {
-      // Validate token
       jwt.verify(req.cookies.token, SECRET_KEY, (err, decoded) => {
         if(err){
           // Invalid token; Delete cookie and redirect to login.
           res.clearCookie('token');
           res.redirect('/login');
-          return
+          return;
         } else {
-          // Token is valid; Allow user to access page.
+          // Set decoded user to request to use in view controller.
+          // controllers will have access to user.token, user.orgid and user.permissions (set in contollers.authentication.js)  
+          req.user = decoded;
           next();
-          return
         }
       })
     } else {
-      // No token; Delete cookie and redirect to login.
+      // No token in cookie.
       res.redirect('/login')
-      return
     }
   }
 }
 
+
 module.exports = {
-  jwtrequired,
+  AuthRequired,
   SECRET_KEY
 }
