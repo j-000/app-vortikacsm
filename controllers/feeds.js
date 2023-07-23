a23aa9dc128f1c2a08dcc0aa9182e82e95bd444a
Joao Oliveira <43117830+j-000@users.noreply.github.com>
2023-07-23 13:52:10 +0100

    Created feed controller logic for create, getAll, getById and delete; Created new /api/feeds and /api/feeds/:feedid routes

--- controllers/feeds.js
+++ controllers/feeds.js
@@ -1,22 +1,80 @@
-const { MongoDB } = require('../services/mongo');
+const { MongoDB } = require('../database/mongo');
+const { ObjectId } = require('mongodb');
+const { Feed } = require('../models/feed');
+
 
 class FeedsController {
-  static async getFeeds(req, res) {
+
+  static async create(req, res){
+    const orgid = req.user.orgid;
+    const { name, url, type} = req.body;
+
+    // Check data required is available
+    if (!name || !url || !type) {
+      res.status(400).json({ error: 'Missing data to create feed. '});
+    }
+
+    // Create new feed and save
+    const newFeed = new Feed(name, url, type, orgid).toJSON();
+
     try {
       const db = await MongoDB.getdb();
       const feedsCollection = db.collection('feeds');
-      const feeds = await feedsCollection.find({ name: 'bu' }).toArray();
+      const result = await feedsCollection.insertOne(newFeed);
+      res.status(201).json({success: true});
+      MongoDB.closedb();
+    } catch (error) {
+      res.status(500).json({error: 'Server error.'});
+    }
+  }
 
-      res.json(feeds);
-
+  static async getAll(req, res) {
+    const orgid = req.user.orgid;
+    try {
+      const db = await MongoDB.getdb();
+      const feedsCollection = db.collection('feeds');
+      let feeds = await feedsCollection.find({ orgid }).toArray();
+      res.status(200).json({feeds});
       MongoDB.closedb();
     } catch (err) {
-      console.error(`Error: ${err}`);
+      res.json({ error: 'Server error.' });
+    }
+  }
+  
+  static async getById(req, res) {
+    const orgid = req.user.orgid;
+    try {
+      const db = await MongoDB.getdb();
+      const feedsCollection = db.collection('feeds');
+      const _id = new ObjectId(req.params.feedid);
+      const feed = await feedsCollection.findOne({ orgid, _id });
+      res.json({feed});
+      MongoDB.closedb();
+    } catch (err) {
+      res.json({ error: err });
+    }
+  }
+  
+  static async update(req, res){
+
+  }
+
+  static async delete(req, res){
+    const orgid = req.user.orgid;
+    try {
+      const db = await MongoDB.getdb();
+      const feedsCollection = db.collection('feeds');
+      const _id = new ObjectId(req.params.feedid);
+      const feed = await feedsCollection.findOneAndDelete({ orgid, _id });
+      res.json({success: true});
+      MongoDB.closedb();
+    } catch (err) {
       res.json({ error: err });
     }
   }
 
 }
+
 
 module.exports = {
   FeedsController
--- routes/api.js
+++ routes/api.js
@@ -3,13 +3,8 @@ const apiRoutes = express.Router()
 const { FeedsController, AuthorizeOrIsOwner } = require('../controllers/feeds');
 const { UsersController } = require('../controllers/users');
 const { AuthRequired } = require('../middlewares/authentication');
-const { RolesController } = require('../controllers/roles');
 const { CheckPermissions, PERMISSIONS } = require('../middlewares/permissions');
 const { AuthenticationController } = require('../controllers/authentication');
-
-
-
-
 
 
 /* /api/register */
@@ -26,6 +21,17 @@ apiRoutes.route('/login')
 apiRoutes.route('/logout')
   .get(AuthenticationController.logout);
 
+
+// /api/feeds
+apiRoutes.route('/feeds')
+  .get(AuthRequired, CheckPermissions(PERMISSIONS.VIEWFEED), FeedsController.getAll)
+  .post(AuthRequired, CheckPermissions(PERMISSIONS.CREATEFEED), FeedsController.create)
+
+
+// /api/feeds/:feedid
+apiRoutes.route('/feeds/:feedid')
+  .get(AuthRequired, CheckPermissions(PERMISSIONS.VIEWFEED), FeedsController.getById)
+  .delete(AuthRequired, CheckPermissions(PERMISSIONS.DELETEFEED), FeedsController.delete)
 
 
 module.exports = {
