17becd8382c50edca01d202f60c1ce682d2eec24
Joao Oliveira <43117830+j-000@users.noreply.github.com>
2023-07-26 09:07:04 +0100

    Global functions refactored

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -43,3 +43,15 @@ function showToast(message){
   document.querySelector('.toast-body').innerHTML = message;
   bootstrap.Toast.getOrCreateInstance(document.querySelector('#liveToast')).show();
 }
+
+// Navigate function
+function navigateTo(url){
+  const orgid = JSON.parse(sessionStorage.getItem('user')).orgid;
+  window.location = url.replace('#ORGID#', orgid);
+}
+
+// Show toast on _t query param set
+const _t = new URLSearchParams(window.location.search).get('_t');
+if (_t) { 
+  showToast(`<span class="text-danger"><i class="fa-solid fa-ban me-2"></i><span>${_t.toUpperCase()}</span></span>`); 
+}
