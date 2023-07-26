995f977fa9fc6be44cb22630c1ad5ab024bc71d8
Joao Oliveira <43117830+j-000@users.noreply.github.com>
2023-07-26 08:45:25 +0100

    Refactored auth controller into usercontroller since it's related to user and updates/fetches user collections

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,8 @@ const { MongoDB } = require('../database/mongo');
 const bcrypt = require('bcrypt');
 const { User } = require('../models/user');
 const { ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const { SECRET_KEY } = require('../middlewares/authentication');
 
 
 class UsersController {
@@ -98,6 +100,53 @@ class UsersController {
       res.status(500).json({error});
     }
   }
+
+  static async logout(req, res) {
+    res.clearCookie('token');
+    res.redirect('/login');
+  }
+
+  static async login(req, res) {   
+    const { email, password } = req.body;
+
+    // Check email and password are present
+    if (!email || !password){
+      return res.status(400).json({error: 'Missing email or password.'})
+    }
+
+    try {
+      const db = await MongoDB.getdb();
+      const usersCollection = db.collection('users');
+
+      // Check if user exists
+      const exists = await usersCollection.findOne({ email });
+      if (exists) {
+        // Check password hash against stored hash
+        const isValidPwd = await bcrypt.compare(password, exists.hashPwd);
+        if (isValidPwd) {
+          const {_id, orgid, permissions} =  exists;
+          const user = { userid: _id, orgid, permissions}
+
+          // Create signed bscypt object with user data
+          const token = jwt.sign(user, SECRET_KEY, { expiresIn: '24h' });
+          
+          // Set cookie token
+          res.cookie('token', token, { httpOnly: true });
+          
+          res.status(200).json({ success: true, token, user });
+          MongoDB.closedb();
+        } else {
+          res.status(403).json({ error: 'Invalid password.' })
+        }
+      } else {
+        res.status(404).json({ error: 'User not registered.' });
+        MongoDB.closedb()
+      }
+    } catch (error) {
+      res.status(500).json({error: 'Server error.'});
+    }
+  }
+
 }
 
 
