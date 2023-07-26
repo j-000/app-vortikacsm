f264037848a3f1cb0ce945bc3bb17317bad0361b
Joao Oliveira <43117830+j-000@users.noreply.github.com>
2023-07-26 09:05:27 +0100

    New permissions

--- middlewares/permissions.js
+++ middlewares/permissions.js
@@ -1,16 +1,44 @@
 const PERMISSIONS = {
 
   // User 
-  VIEWUSER: 'view-user',
-  CREATEUSER: 'create-user',
-  UPDATEUSER: 'update-user',
-  DELETEUSER: 'delete-user',
+  VIEW_USER: 'view-user',
+  CREAT_EUSER: 'create-user',
+  UPDATE_USER: 'update-user',
+  DELETE_USER: 'delete-user',
   
   // Feed 
-  VIEWFEED: 'view-feed',
-  CREATEFEED: 'create-feed',
-  UPDATEFEED: 'update-feed',
-  DELETEFEED: 'delete-feed',
+  VIEW_FEED: 'view-feed',
+  CREATE_FEED: 'create-feed',
+  UPDATE_FEED: 'update-feed',
+  DELETE_FEED: 'delete-feed',
+
+  // CMS
+  // Templates
+  VIEW_BASIC_TEMPLATE: 'view-basic-template',
+  CREATE_BASIC_TEMPLATE: 'create-basic-template',
+  UPDATE_BASIC_TEMPLATE: 'update-basic-template',
+  DELETE_BASIC_TEMPLATE: 'delete-basic-template',
+
+  VIEW_ADVANCED_TEMPLATE: 'view-advanced-template',
+  CREATE_ADVANCED_TEMPLATE: 'create-advanced-template',
+  UPDATE_ADVANCED_TEMPLATE: 'update-advanced-template',
+  DELETE_ADVANCED_TEMPLATE: 'delete-advanced-template',
+
+  VIEW_SEARCH_RESULTS_TEMPLATE: 'view-search-results-template',
+  CREATE_SEARC_HRESULTS_TEMPLATE: 'create-search-results-template',
+  UPDATE_SEARCH_RESULTS_TEMPLATE: 'update-search-results-template',
+  DELETE_SEARCHR_ESULTS_TEMPLATE: 'delete-search-results-template',
+  
+  // Publishing
+  VIEW_PUBLISH_PREVIEW: 'view-publish-preview',
+  UPDATE_PUBLISH_PREVIEW: 'update-publish-preview',
+  ALLOW_PULISH_PREVIEW: 'allow-publish-preview',
+
+  VIEW_PUBLISH_LIVE: 'view-publish-live',
+  UPDATE_PUBLISH_LIVE: 'view-publish-live',
+  ALLOW_PUBLISH_LIVE: 'view-publish-live',
+
+  
 
 }
 
