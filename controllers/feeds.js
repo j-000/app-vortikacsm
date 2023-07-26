79464a8de2009b9bd64101e0d10de7f18565a4b0
Joao Oliveira <43117830+j-000@users.noreply.github.com>
2023-07-26 08:43:06 +0100

    Refactored code from importer controller to feedcontroller since this code is fetching feeds collection

--- controllers/feeds.js
+++ controllers/feeds.js
@@ -2,6 +2,7 @@ const { MongoDB } = require('../database/mongo');
 const { ObjectId } = require('mongodb');
 const { Feed } = require('../models/feed');
 const { getMappingsTemplateObject } = require('../models/mappings');
+const { importJobs } = require('../services/importer');
 
 
 class FeedsController {
@@ -84,6 +85,52 @@ class FeedsController {
       MongoDB.closedb();
     } catch (err) {
       res.json({ error: err });
+    }
+  }
+
+  static async runImport(req, res) {
+    const orgid = req.user.orgid;
+    let _id;
+    
+    try {
+      _id = new ObjectId(req.params.feedid);
+    } catch (err) {
+      res.status(400).json({error: 'Invalid feedid.'})
+      return
+    }
+
+    try {
+      const db = await MongoDB.getdb();
+      const feedsCollection = db.collection('feeds');
+
+      // Check if feed exists
+      const feed = await feedsCollection.findOne({ _id, orgid});
+      if (feed) {
+        MongoDB.closedb()
+
+        // Check feed has not run in the last 1h
+        if (feed.lastImport){
+          const oneHourInMs = 3600000;
+          const differenceMs = Date.now() - feed.lastImport;
+          if (differenceMs < oneHourInMs) {
+            res.status(403).json({ error: 'Importer run less than 1h ago.'})
+            return
+          } else {
+            importJobs(feed, orgid)
+            res.status(200).json({ success: 'Import started.' });
+          }
+
+        } else {
+          importJobs(feed, orgid)
+          res.status(200).json({ success: 'Import started.' });
+        }
+      
+      } else {
+        res.status(404).json({ error: 'No feed found.' })
+      }
+
+    } catch (error) {
+      res.status(500).json({ error });
     }
   }
 
