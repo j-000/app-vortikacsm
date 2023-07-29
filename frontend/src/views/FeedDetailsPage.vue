e6621f89673eea115543848367e37fceb992c8d5
Joao Oliveira <43117830+j-000@users.noreply.github.com>
2023-07-29 13:55:08 +0100

    Covers #7

--- frontend/src/views/FeedDetailsPage.vue
+++ frontend/src/views/FeedDetailsPage.vue
@@ -1,92 +1,61 @@
 <template>
-  <div>
-    <FeedCard :feedid="$route.params.feedid"/>
-
+  <div class="mb-5">
+    <a @click="runImporter" class="btn btn-primary mb-3" href="#">Run Importer</a>
+    <FeedCard :feedid="$route.params.feedid" />
     <div class="row">
       <div class="col">
-        <div class="card mt-4">
-          <div class="card-header">
-            <div class="col">
-              <div class="d-flex justify-content-between">
-                <h5 class="mb-1 card-title">Source Fields</h5>
-              </div>
-            </div>
-          </div>
-          <div class="card-body" id="sourceFieldsList" style="height: 15em; overflow-y: scroll;">
-            
-          </div>
-        </div>
+        <SourceFields :feedid="$route.params.feedid" />
       </div>
       <div class="col">
-        <div class="card mt-4">
-          <div class="card-header">
-            <div class="col">
-              <div class="d-flex justify-content-between">
-                <h5 class="mb-1 card-title">Mappings <span id="mappingsCompleted"></span></h5>
-                <button class="btn btn-primary btn-sm">Save</button>
-              </div>
-            </div>
-          </div>
-          <div class="card-body p-0" style="height: 15em; overflow-y: scroll;">
-            <div class="accordion" id="accordionExample">
-            </div>
-          </div>
-        </div>
+        <MappingFields :feedid="$route.params.feedid" />
       </div>
     </div>
-    <div class="card mt-4">
-      <div class="card-header">
-        <div class="col">
-          <div class="d-flex justify-content-between">
-            <h5 class="mb-1 card-title">Jobs</h5>
-          </div>
-        </div>
-      </div>
-      <div class="card-body">
-        Paginated jobs here...
-        <nav aria-label="Page navigation example">
-          <ul class="pagination justify-content-center mb-0">
-            <li class="page-item disabled">
-              <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
-            </li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item">
-              <a class="page-link" href="#">Next</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
-    <div class="card mt-4">
-      <div class="card-header">
-        <div class="col">
-          <div class="d-flex justify-content-between">
-            <h5 class="mb-1 card-title">History</h5>
-          </div>
-        </div>
-      </div>
-      <div class="card-body">
-        Feed import history here...
-      </div>
-    </div>
+    <FeedJobs :feedid="$route.params.feedid" />
+    <ImportHistory :feedid="$route.params.feedid" />
   </div>
 </template>
 
 <script>
-import FeedCard from '../components/FeedCard.vue';
+import { useRoute } from "vue-router";
+import toast from "../functions";
+import global from "../stores/global";
+import FeedCard from "../components/FeedCard.vue";
+import SourceFields from "../components/SourceFields.vue";
+import MappingFields from "../components/MappingFields.vue";
+import FeedJobs from "../components/FeedJobs.vue";
+import ImportHistory from "../components/ImportHistory.vue";
 
 export default {
-  components:{
-    FeedCard
+  components: {
+    FeedCard,
+    MappingFields,
+    SourceFields,
+    FeedJobs,
+    ImportHistory
   },
-  setup(){
+  setup() {
+    const store = global();
+    const router = useRoute();
+    
 
-  }
-}
+    const runImporter = async () => {
+      const response = await fetch(`http://localhost:3001/api/feeds/${router.params.feedid}/run-import`, { 
+        method: 'post',
+        headers: { authorization: `Bearer ${store.user.token}`}});
+      const json = await response.json()
+      if(json.success){
+        toast(json.success);
+      } else {
+        toast(json.error);
+      }
+    }
+
+    return {
+      runImporter
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
