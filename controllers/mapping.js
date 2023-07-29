db50c086db5dbd819bb1a4533e84b779dfbe28a8
Joao Oliveira <43117830+j-000@users.noreply.github.com>
2023-07-29 13:48:34 +0100

    use service; new control function for update

--- controllers/mapping.js
+++ controllers/mapping.js
@@ -1,20 +1,18 @@
 const { ObjectId } = require("mongodb");
-const { MongoDB } = require("../database/mongo");
-
+const MappingsService = require('../database/services/mappings');
 
 class MappingController {
-  static async getMappings(req, res) {
+  static async getAll(req, res) {
     const orgid = req.user.orgid;
     const feedid = new ObjectId(req.params.feedid);
     try {
-      const db = await MongoDB.getdb();
-      const mappings = await db.collection('mappings').findOne({ feedid }, { projection: { _id: 0, feedid: 0 } });
+      const mappings = await MappingsService.getOne({ feedid }, { __v: 0 });
       let requiredMappingComplete = true
 
-      Object.entries(mappings).forEach(([k, v]) => {
+      Object.entries(mappings.props).forEach(([k, v]) => {
         if (typeof v === 'object'){
-          if (v.required) {
-            if (!v.mappedTo.sourceField && !v.mappedTo.function) {
+          if (v.isRequired) {
+            if (v.mappedTo.sourceField === null && v.mappedTo.function === null) {
               requiredMappingComplete = false
               return
             }
@@ -22,8 +20,20 @@ class MappingController {
         }
       })
 
-      res.json({ mappings, requiredMappingComplete })
+      res.json({ success: true, mappings, requiredMappingComplete })
     } catch (error) {
+      res.json(error);
+    }
+  }
+
+  static async update(req, res){
+    const orgid = req.user.orgid;
+    const feedid = new ObjectId(req.params.feedid);
+    const props = req.body.props;
+    try {
+      const mappings = await MappingsService.update({ feedid }, { props });
+      res.json({success: true})
+    } catch(error){
       res.json(error);
     }
   }
