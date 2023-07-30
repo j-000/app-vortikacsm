3b96e215cbadbb72b9d60c623fd73a505469d27a
Joao Oliveira <43117830+j-000@users.noreply.github.com>
2023-07-30 23:04:10 +0100

    New routing middleware to ensure users see eithe preview or live site

---
+++ middlewares/routing.js
@@ -0,0 +1,28 @@
+function setsubdomain(req, res, next) { 
+    const liveSiteUrl = process.env.LIVE_DOMAIN;
+  
+    // Set flag to 'public' so it always goes to live site by default
+    // This should match the same name as the public/ templates folder
+    req._subdomain = 'public';
+  
+    // check if a subdomain is being accessed
+    if (req.hostname.includes('.')){
+
+      // Only "preview" subdomain is allowed.
+      if (req.hostname === process.env.PREVIEW_DOMAIN){
+        // Set flag to 'preview'
+        // This should match the same name as the preview/ templates folder
+        req._subdomain = 'preview';
+      } else {
+        // If it's anything else other than preview, redirect to main site (public);
+        res.redirect(liveSiteUrl);
+        return
+      }
+    }
+    // Continue with route
+    next();
+}
+
+module.exports = {
+    setsubdomain
+}
