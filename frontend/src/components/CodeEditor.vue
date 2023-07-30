f86e0943d46c613fb4cf84a35ce838200655880a
Joao Oliveira <43117830+j-000@users.noreply.github.com>
2023-07-30 23:12:29 +0100

    Code editor and Nav updates

--- frontend/src/components/CodeEditor.vue
+++ frontend/src/components/CodeEditor.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    <a href="#" class="btn btn-primary mb-3">Save</a>
     <div id="editor"></div>
+    <div class="mt-3 d-flex justify-content-between">
+      <p></p>
+      <button class="btn-primary btn" @click="saveFile">Save</button>
+    </div>
   </div>
 </template>
 
 <script>
 import global from '../stores/global';
+import toast from '../functions';
 
 export default {
   props: {
@@ -14,40 +18,63 @@ export default {
   },
   setup(props) {
     const store = global();
-    
-    const editorValue = () => {
-      var editor = window.ace.edit("editor");
-      return editor.getValue();
-    }
 
-    const setValue = (v) => {
-        var editor = window.ace.edit("editor");
-        editor.setValue(v);
-    }
-
-    const setTemplateContent = async () => {
+    const initAceEditor = async () => {
+      const editor = window.ace.edit('editor');
+      editor.setTheme('ace/theme/monokai');
+      editor.session.setMode('ace/mode/html');
+      editor.setOption("showPrintMargin", false);
       const response = await fetch(`http://localhost:3001/api/cms/templates/${props.templateid}`, {
         headers: { authorization: `Bearer ${store.user.token}`}
       });
       const json = await response.json();
-      setValue(json.templateContent);
+      if (json.error){
+        disableEditor();
+        toast(json.error);
+      }
+      setValue(json.fileContent);
     }
-
-    const initAceEditor = () => {
-      const editor = window.ace.edit('editor');
-      editor.setTheme('ace/theme/monokai');
-      editor.session.setMode('ace/mode/html');
-      editor.setOption("showPrintMargin", false)
-      setTemplateContent();
-    }
-    
     const script = document.createElement('script');
     script.src = 'https://cdn.jsdelivr.net/npm/ace-builds@1.23.4/src-min-noconflict/ace.min.js';
     script.async = true;
     script.onload = initAceEditor;
     document.body.appendChild(script);
     
+     
+    const editorValue = () => {
+      var editor = window.ace.edit("editor");
+      return editor.getValue();
+    }
 
+    const setValue = (v) => {
+      var editor = window.ace.edit("editor");
+      editor.setValue(v);
+    }
+
+    const disableEditor = () => {
+      var editor = window.ace.edit("editor");
+      editor.setReadOnly(true);
+    }
+    
+    const saveFile = async () => {
+      const response = await fetch(`http://localhost:3001/api/cms/templates/${props.templateid}`, {
+        method: 'put',
+        headers: { authorization: `Bearer ${store.user.token}`, 'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          newContent: editorValue()
+        })
+      });
+      const json = await response.json();
+      if (json.error){
+        toast(json.error);
+      } else {
+        toast(json.message);
+      }
+    }
+    
+    return {
+      saveFile
+    }
   }
 }
 </script>
@@ -58,5 +85,11 @@ export default {
     height: 500px;
     width: 100%;
 }
+.ace_editor {
+  font-size: 15px!important;
+}
 
+.btn {
+  width: 7em;
+}
 </style>
--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -5,166 +5,155 @@ const store = globalStore();
 </script>
 <template>
   <nav class="navbar navbar-expand-lg">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="#"><i class="fas fa-tornado"></i><span class="ms-2">Vortika<sup>CSM</sup></span></a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse justify-content-between" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                  <div 
-                    v-if="store.user?.token"
-                    class="navbar-nav"
-                  >
-                    <li class="nav-item">
-                      <RouterLink class="nav-link" :to="{name: 'dashboard'}">Dashboard</RouterLink>
-                    </li>
-                    <li class="nav-item">
-                      <RouterLink class="nav-link" :to="{name: 'feeds'}">Feeds</RouterLink>
-                    </li>
-                    <li class="nav-item">
-                      <RouterLink class="nav-link" :to="{name: 'jobs'}">Jobs</RouterLink>
-                    </li>
-                    <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        CMS
-                      </a>
-                      <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><h6 class="dropdown-header">View Templates</h6></li>
-                        <li>
-                          <RouterLink class="dropdown-item d-flex justify-content-between" :to="{name: 'cms-edit-bdp'}">
-                            <div>
-                              <i class="fa-regular fa-file-lines me-2"></i>
-                              <span>Basic Details Page</span>
-                            </div>
-                          </RouterLink>
-                        </li>
-                        <li>
-                          <RouterLink class="dropdown-item d-flex justify-content-between" :to="{name: 'cms-edit-adp'}">
-                            <div>
-                              <i class="fa-regular fa-file-lines me-2"></i>
-                              <span>Advanced Details Page</span>
-                            </div>
-                          </RouterLink>
-                        </li>
-                        <li>
-                          <RouterLink class="dropdown-item d-flex justify-content-between" :to="{name: 'cms-edit-srp'}">
-                            <div>
-                              <i class="fa-regular fa-file-lines me-2"></i>
-                              <span>Search Results Page</span>
-                            </div>
-                          </RouterLink>
-                        </li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><h6 class="dropdown-header">View Sites</h6></li>
-                        <li>
-                          <RouterLink class="dropdown-item d-flex justify-content-between" :to="{name: 'cms-publish-preview'}">
-                            <div>
-                              <i class="fas fa-flask me-2"></i>
-                              <span>Preview</span>
-                            </div>
-                          </RouterLink>
-                        </li>
-                        <li>
-                          <RouterLink class="dropdown-item d-flex justify-content-between" :to="{name: 'cms-publish-live'}">
-                            <div>
-                              <i class="fas fa-globe me-2"></i>
-                              <span>Live</span>
-                            </div>
-                            <div>
-                            </div>
-                          </RouterLink>
-                        </li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><h6 class="dropdown-header">Publish Templates</h6></li>
-                        <li>
-                          <RouterLink class="dropdown-item d-flex justify-content-between" :to="{name: 'cms-publish-preview'}">
-                            <div>
-                              <i class="fas fa-flask me-2"></i>
-                              <span>Preview</span>
-                            </div>
-                            <div>
-                              <span id="orangeDot"></span>
-                            </div>
-                          </RouterLink>
-                        </li>
-                        <li>
-                          <RouterLink class="dropdown-item d-flex justify-content-between" :to="{name: 'cms-publish-live'}">
-                            <div>
-                              <i class="fas fa-globe me-2"></i>
-                              <span>Live</span>
-                            </div>
-                            <div>
-                              <span id="greenPulseDot"></span>
-                            </div>
-                          </RouterLink>
-                        </li>
-                      </ul>
-                    </li>
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><i class="fas fa-tornado"></i><span class="ms-2">Vortika<sup>CSM</sup></span></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-between" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <div 
+            v-if="store.user?.token"
+            class="navbar-nav"
+          >
+            <li class="nav-item">
+              <RouterLink class="nav-link" :to="{name: 'dashboard'}">Dashboard</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" :to="{name: 'feeds'}">Feeds</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" :to="{name: 'jobs'}">Jobs</RouterLink>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Career Site Manager
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><h6 class="dropdown-header">View Templates</h6></li>
+                <li>
+                  <RouterLink class="align-items-center dropdown-item d-flex justify-content-between" :to="{name: 'draft-pages'}">
+                    <div>
+                      <i class="fa-regular fa-file-lines me-2"></i>
+                      <span>Drafts</span>
+                    </div>
+                    <span id="grayDot"></span>
+                  </RouterLink>
+                </li>
+                <li>
+                  <RouterLink class="align-items-center dropdown-item d-flex justify-content-between" :to="{name: 'preview-pages'}">
+                    <div>
+                      <i class="fa-regular fa-file-lines me-2"></i>
+                      <span>Preview</span>
+                    </div>
+                    <span id="orangeDot"></span>
+                  </RouterLink>
+                </li>
+                <li>
+                  <RouterLink class="align-items-center dropdown-item d-flex justify-content-between" :to="{name: 'live-pages'}">
+                    <div>
+                      <i class="fa-regular fa-file-lines me-2"></i>
+                      <span>Live</span>
+                    </div>
+                    <span id="greenDot"></span>
+                  </RouterLink>
+                </li>
+                <li><hr class="dropdown-divider"></li>
+                <li><h6 class="dropdown-header">View Sites</h6></li>
+                <li>
+                  <RouterLink class="dropdown-item d-flex justify-content-between" :to="{name: 'home'}">
+                    <div>
+                      <i class="fas fa-flask me-2"></i>
+                      <span>Preview</span>
+                    </div>
+                  </RouterLink>
+                </li>
+                <li>
+                  <RouterLink class="dropdown-item d-flex justify-content-between" :to="{name: 'home'}">
+                    <div>
+                      <i class="fas fa-globe me-2"></i>
+                      <span>Live</span>
+                    </div>
+                    <div>
+                    </div>
+                  </RouterLink>
+                </li>
+                <li><hr class="dropdown-divider"></li>
+                <li><h6 class="dropdown-header">Learn</h6></li>
+                <li>
+                  <RouterLink class="align-items-center dropdown-item d-flex justify-content-between" :to="{name: 'learn'}">
+                    <div>
+                      <i class="fa-solid fa-book me-2"></i>
+                      <span>Vortika<sup>CSM</sup></span>
+                    </div>
+                  </RouterLink>
+                </li>
+              </ul>
+            </li>
+          </div>
+          <div 
+            v-else
+            class="navbar-nav"
+          >
+            <li class="nav-item">
+              <RouterLink class="nav-link" :to="{name: 'home'}">Home</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" :to="{name: 'register'}">Register</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" :to="{name: 'login'}">Login
+                </RouterLink>
+            </li>
+          </div>
+        </ul>
+        <div 
+          v-if="store.user?.token"
+          class="navbar-nav"
+        >
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Menu</a>
+            <ul class="dropdown-menu">
+              <li>
+                <RouterLink class="dropdown-item d-flex justify-content-between" :to="{name: 'login'}">
+                  <div>
+                    <i class="fa-solid fa-user"></i>
+                    <span class="ms-2">Profile</span>
                   </div>
-                  <div 
-                    v-else
-                    class="navbar-nav"
-                  >
-                    <li class="nav-item">
-                      <RouterLink class="nav-link" :to="{name: 'home'}">Home</RouterLink>
-                    </li>
-                    <li class="nav-item">
-                      <RouterLink class="nav-link" :to="{name: 'register'}">Register</RouterLink>
-                    </li>
-                    <li class="nav-item">
-                      <RouterLink class="nav-link" :to="{name: 'login'}">Login
-                        </RouterLink>
-                    </li>
+                </RouterLink>
+              </li>
+              <li>
+                <RouterLink class="dropdown-item d-flex justify-content-between" :to="{name: 'login'}">
+                  <div>
+                    <i class="fa-solid fa-gears"></i>
+                    <span class="ms-2">Settings</span>
                   </div>
-                </ul>
-                <div 
-                  v-if="store.user?.token"
-                  class="navbar-nav"
-                >
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Menu</a>
-                    <ul class="dropdown-menu">
-                      <li>
-                        <RouterLink class="dropdown-item d-flex justify-content-between" :to="{name: 'login'}">
-                          <div>
-                            <i class="fa-solid fa-user"></i>
-                            <span class="ms-2">Profile</span>
-                          </div>
-                        </RouterLink>
-                      </li>
-                      <li>
-                        <RouterLink class="dropdown-item d-flex justify-content-between" :to="{name: 'login'}">
-                          <div>
-                            <i class="fa-solid fa-gears"></i>
-                            <span class="ms-2">Settings</span>
-                          </div>
-                        </RouterLink>
-                      </li>
-                      <li>
-                        <RouterLink class="dropdown-item d-flex justify-content-between" :to="{name: 'login'}">
-                          <div>
-                            <i class="fa-solid fa-users"></i>
-                            <span class="ms-2">Users</span>
-                          </div>
-                        </RouterLink>
-                      </li>
-                      
-                      <hr>
-                      <li>
-                        <RouterLink class="dropdown-item d-flex justify-content-between text-danger" :to="{name: 'logout'}">
-                          <div>
-                            <i class="fa-solid fa-right-from-bracket"></i>
-                            <span class="ms-2">Logout</span>
-                          </div>
-                        </RouterLink>
-                      </li>
-                    </ul>
-                  </li>
-                </div>
-              </div>
-            </div>
-          </nav>
+                </RouterLink>
+              </li>
+              <li>
+                <RouterLink class="dropdown-item d-flex justify-content-between" :to="{name: 'login'}">
+                  <div>
+                    <i class="fa-solid fa-users"></i>
+                    <span class="ms-2">Users</span>
+                  </div>
+                </RouterLink>
+              </li>
+              
+              <hr>
+              <li>
+                <RouterLink class="dropdown-item d-flex justify-content-between text-danger" :to="{name: 'logout'}">
+                  <div>
+                    <i class="fa-solid fa-right-from-bracket"></i>
+                    <span class="ms-2">Logout</span>
+                  </div>
+                </RouterLink>
+              </li>
+            </ul>
+          </li>
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
