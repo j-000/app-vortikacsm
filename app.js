d22f50858b8d50a677ce138e4f043540b852f2f0
Joao Oliveira <43117830+j-000@users.noreply.github.com>
2023-07-30 23:06:31 +0100

    refactored; added nunjucks templating engine setup; dotenv setup; new middlewares;

--- app.js
+++ app.js
@@ -1,51 +1,65 @@
-// Create App
+// ExpressJs imports
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const { logrequests } = require('./middlewares/serverlogging');
 const mongoose = require('mongoose');
+const nunjucks = require('nunjucks');
+
+// Import custom middleware
+const { logrequests } = require('./middlewares/serverlogging');
+const { setsubdomain } = require('./middlewares/routing');
+
+// Import custom routes
+const { apiRoutes } = require('./routes/api');
+const { coreRoutes } = require('./routes/core');
 
 
+// Create express.js app
+const app = express();
+
+// Load environment variables
+require('dotenv').config()
+
+
+// Connect to DB
 main().catch(err => console.log(err));
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/database1');
 }
 
+// Set view engine to Nunjucks
+app.set('view engine', 'njk');
+nunjucks.configure('templates', {
+  autoescape: true,
+  express: app,
+});
 
 // Set templates folder
-app.set('views', path.join(__dirname, 'templates'));
+app.set('views', path.join(__dirname, 'templates/'));
 
-
-// Use to parse JSON requests
+// Use bodyparser to parse JSON requests
 app.use(bodyParser.json())
-
 
 // Set static folder
 app.use(express.static('public'));
 
-
-// [Must stay on top of app.js and before middleware below]
 // Allow CORS 
+// [Must stay on top of app.js and before middleware below]
 app.use(cors({ origin: ['http://localhost:5173'] }));
-
 
 // Cookie Parser
 app.use(cookieParser());
-
 
 // [Must stay on top of app.js] 
 // Custom middleware for request logging - #DEVONLY
 app.use((req, res, next) => logrequests(req, res, next))
 
+// Middleware to ensure corret stage (public/preview) is set.
+app.use((req, res, next) => setsubdomain(req, res, next));
 
-// Import Routes
-const { apiRoutes } = require('./routes/api');
-
-
-// Use Routes
+app.use('/', coreRoutes);
 app.use('/api', apiRoutes);
 
 
