da5111fae050b256b2c2b65a62b8070f67b40fd4
Joao Oliveira <43117830+j-000@users.noreply.github.com>
2023-07-31 23:44:39 +0100

    #8; new mappings route to get all mappings

--- controllers/mapping.js
+++ controllers/mapping.js
@@ -4,6 +4,35 @@ const MappingsService = require('../database/services/mappings');
 class MappingController {
   static async getAll(req, res) {
     const orgid = req.user.orgid;
+    try {
+      const mappings = await MappingsService.getMany({ orgid }, { __v: 0 });
+      let allMappings = [];
+      
+      mappings.forEach( mappingDoc => {
+        let temp = {}
+        let requiredMappingComplete = true;
+        
+        Object.entries(mappingDoc.props).forEach(([k, v]) => {
+          if (typeof v === 'object'){
+            if (v.isRequired) {
+              if (v.mappedTo.sourceField === null && v.mappedTo.function === null) {
+                requiredMappingComplete = false
+                return
+              }
+            }
+          }
+        });
+        temp = { requiredMappingComplete, ...mappingDoc.toJSON() }
+        allMappings.push(temp);
+      })
+      res.json({ success: true, allMappings })
+    } catch (error) {
+      res.json(error);
+    }
+  }
+
+  static async getByFeedid(req, res) {
+    const orgid = req.user.orgid;
     const feedid = new ObjectId(req.params.feedid);
     try {
       const mappings = await MappingsService.getOne({ feedid }, { __v: 0 });
