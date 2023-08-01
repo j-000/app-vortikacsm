2b020416c44ced5e3b06cf5d91d08d92ec345f71
Joao Oliveira <43117830+j-000@users.noreply.github.com>
2023-08-01 21:20:06 +0100

    #29;

--- frontend/src/views/cms/DraftPages.vue
+++ frontend/src/views/cms/DraftPages.vue
@@ -4,8 +4,8 @@
       <div class="col d-flex justify-content-between">
         <h1 class="h3 text-gray-800">Pages in drafts <span id="grayDot"></span></h1>
         <div>
-          <button class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#newTemplateModal">
-            <i class="fas fa-plus"></i> <span class="ml-3">New Draft</span>
+          <button class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#newpagemodal">
+            <i class="fas fa-plus"></i> <span class="ml-3">New page</span>
           </button>
         </div>
       </div>
@@ -38,28 +38,50 @@
       </tbody>
     </table>
 
-    <div class="modal fade" id="newTemplateModal" tabindex="-1" role="dialog" aria-labelledby="newTemplateModal" aria-hidden="true" >
+    <div class="modal fade" id="newpagemodal" tabindex="-1" role="dialog" aria-labelledby="newpagemodal" aria-hidden="true" >
       <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="newTemplateModal">Add a new draft page</h5>
-            <button type="button" id="newFeedCloseButton" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <h5 class="modal-title">Add a new page</h5>
+            <button type="button" id="newpagemodalCloseBtn" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
+          
           <div class="modal-body">
-            <form class="feed">
-              <div class="form-group">
-                <div class="input-group mb-3">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text" id="basic-addon3">Name</span>
-                  </div>
-                  <input v-model="newFileName" type="text" class="form-control" id="name" aria-describedby="basic-addon3"/>
-                </div>
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text">Name</span>
               </div>
-            </form>
+              <input v-model="newPage.name" type="text" class="form-control" aria-describedby="page-name"/>
+              <div class="input-group-prepend">
+                <span class="input-group-text">.html</span>
+              </div>
+            </div>
+            <p class="mb-3 ms-1"><small class="text-muted">Only characters (a - z, A - Z), numbers (0 - 9) and underscores (_) allowed.</small></p>
+
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text" for="type">Page type</span>
+              </div>
+              <select v-model="newPage.pageType" class="form-control" aria-describedby="page-type">
+                <option selected>Choose...</option>
+                <option value="content">Content</option>
+                <option value="template">Template</option>
+                <option value="jobdetails">Job details</option>
+                <option value="searchresults">Search results</option>
+              </select>
+            </div>
+            <div v-if="newPage.pageType == 'content'" class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text">{{ store.domains.live }}/</span>
+              </div>
+              <input v-model="newPage.urlslug" type="text" class="form-control" aria-describedby="page-slug"/>
+            </div>
+            <p class="mb-3 ms-1"><small class="text-muted">Only lowercase characters (a - z), numbers (0 - 9) and dashes (-) allowed.</small></p>
+
           </div>
           <div class="modal-footer">
             <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">Cancel</button>
-            <button class="btn btn-primary" type="button" @click="addNewDraftPage">Save</button>
+            <button class="btn btn-primary" type="button" @click="addNewPage">Save</button>
           </div>
         </div>
       </div>
@@ -76,11 +98,15 @@ export default {
   setup() {
     const store = global();
     const files = ref({});
-    const newFileName = ref('');
-    const newFileType = ref('');
+    const newPage = ref({
+      name: '',
+      pageType: '',
+      urlslug: '',
+      status: 'draft'
+    })
 
     const getFiles = async () => {
-      const response = await fetch(`http://localhost:3001/api/cms/templates/list?status=draft`, {
+      const response = await fetch(`http://localhost:3001/api/cms/pages/list?status=draft`, {
         headers: { authorization: `Bearer ${store.user.token}`}
       });
       const json = await response.json();
@@ -88,33 +114,30 @@ export default {
     }
     getFiles();
 
-    const addNewDraftPage = async () => {
-      const response = await fetch('http://localhost:3001/api/cms/templates/list', {
+    const addNewPage = async () => {
+      const response = await fetch('http://localhost:3001/api/cms/pages/list', {
         method: 'post', 
         headers: {
           authorization: `Bearer ${store.user.token}`,
           'Content-Type': 'application/json'
         }, 
-        body: JSON.stringify({
-          name: newFileName.value, 
-          status: 'draft'
-        })});
+        body: JSON.stringify(newPage.value)});
       const json = await response.json();
 
       if (json.success) {
         getFiles();
         toast(json.message);
-        document.querySelector('#newFeedCloseButton').click();
+        document.querySelector('#newpagemodalCloseBtn').click();
       } else {
         toast(json.error);
       }
     }
 
     return {
+      store,
       files,
-      newFileName,
-      newFileType,
-      addNewDraftPage
+      newPage,
+      addNewPage
     }
   }
 }
@@ -123,6 +146,10 @@ export default {
 <style scoped>
 li {
   list-style: none;
+}
+
+span.input-group-text {
+  width: max-content;
 }
 
 </style>
