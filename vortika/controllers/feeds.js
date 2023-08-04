e5b618e291080244cb2952ce60406abe207d0d91
Joao Oliveira <43117830+j-000@users.noreply.github.com>
2023-08-04 21:53:16 +0100

    Removed unused code; udpated to add job to queue

--- vortika/controllers/feeds.js
+++ vortika/controllers/feeds.js
@@ -29,10 +29,8 @@ class FeedsController {
     const orgid = req.user.orgid;
     try {
       let feeds = await FeedService.getMany({ orgid }, {_id: 1, name: 1});
-
       res.status(200).json({feeds});
     } catch (err) {
-      console.log(err);
       res.json({ error: 'Server error.' });
     }
   }
@@ -65,53 +63,8 @@ class FeedsController {
   }
 
   static async runImport(req, res) {
-    // const orgid = req.user.orgid;
-
-
     jobsQ.add(`Feed-${req.params.feedid}`, { feedid: req.params.feedid});
     res.send({success: true})
-    // let _id;
-    // try {
-    //   _id = new ObjectId(req.params.feedid);
-    // } catch (err) {
-    //   res.status(400).json({error: 'Invalid feedid.'})
-    //   return
-    // }
-
-    // let startRun = false;
-    // try {
-    //   // Check if feed exists
-    //   const feed = await FeedService.getOne({ _id, orgid});
-    //   if (feed) {
-    //     // Check feed has not run in the last 1h
-    //     if (feed.lastImport){
-    //       const oneHourInMs = 3600000;
-    //       const differenceMs = Date.now() - feed.lastImport;
-    //       if (differenceMs < oneHourInMs) {
-    //         res.status(403).json({ error: 'Importer run less than 1h ago.'})
-    //         return
-    //       } else {
-    //         startRun = true;
-    //         res.status(200).json({ success: 'Import started.' });
-    //       }
-
-    //     } else {
-    //       startRun = true;
-    //       res.status(200).json({ success: 'Import started.' });
-    //     }
-
-    //     // Start Importer
-    //     if (startRun) {
-    //       importJobs(feed, orgid)
-    //     };
-
-    //   } else {
-    //     res.status(404).json({ error: 'No feed found.' })
-    //   }
-
-    // } catch (error) {
-    //   res.status(500).json({ error });
-    // }
   }
 
 }
