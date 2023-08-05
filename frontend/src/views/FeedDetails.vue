6546d99d824a02eab052724ca3a9997a385f29d5
Joao Oliveira <43117830+j-000@users.noreply.github.com>
2023-08-05 18:42:58 +0100

    source fields logic

--- frontend/src/views/FeedDetails.vue
+++ frontend/src/views/FeedDetails.vue
@@ -4,14 +4,30 @@
     <FeedCard :feedid="$route.params.feedid" />
     <div class="row">
       <div class="col">
-        <SourceFields :feedid="$route.params.feedid" />
+        <div class="card mt-4">
+          <div class="card-header">
+            <div class="col">
+              <div class="d-flex justify-content-between">
+                <h5 class="mb-1 card-title">Source Fields</h5>
+              </div>
+            </div>
+          </div> 
+          <div class="card-body" id="sourceFieldsList" style="height: 20em; overflow-y: scroll">
+          <ul v-if="feed">
+            <li v-for="sf in feed.sourceFields" :key="sf">{{ sf }}</li>
+          </ul>
+          </div>
+        </div>
       </div>
       <div class="col">
-        <MappingFields :feedid="$route.params.feedid" />
+        <MappingFields v-if="feed" :feedObj="feed" />
       </div>
     </div>
-    <FeedJobs :feedid="$route.params.feedid" />
-    <ImportHistory :feedid="$route.params.feedid" />
+    <div class="row">
+      <div class="col">
+        <FeedJobs v-if="feed" :feedid="feed._id" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,23 +36,35 @@ import { useRoute } from "vue-router";
 import toast from "../functions";
 import global from "../stores/global";
 import FeedCard from "../components/FeedCard.vue";
-import SourceFields from "../components/SourceFields.vue";
 import MappingFields from "../components/MappingFields.vue";
 import FeedJobs from "../components/FeedJobs.vue";
-import ImportHistory from "../components/ImportHistory.vue";
+import { ref } from "vue";
 
 export default {
   components: {
     FeedCard,
     MappingFields,
-    SourceFields,
-    FeedJobs,
-    ImportHistory
+    FeedJobs
   },
   setup() {
     const store = global();
     const router = useRoute();
+    const feed = ref();
+    const route = useRoute()
     
+    const getFeed = async () => {
+      const response = await fetch(
+        `http://localhost:3001/api/feeds/${route.params.feedid}`,
+        { headers: { authorization: `Bearer ${store.user.token}` } }
+      );
+      const json = await response.json();
+      if (json.feed) {
+        feed.value = json.feed;
+      } else {
+        toast(json.error);
+      }
+    }
+    getFeed();
 
     const runImporter = async () => {
       const response = await fetch(`http://localhost:3001/api/feeds/${router.params.feedid}/run-import`, { 
@@ -51,7 +79,8 @@ export default {
     }
 
     return {
-      runImporter
+      runImporter,
+      feed
     };
   },
 };
