ce771aeba3ce5295f711d7237535699fc4cb3ef9
Joao Oliveira <43117830+j-000@users.noreply.github.com>
2023-08-09 19:02:59 +0100

    Importer completed for API/XML

--- importer/app.js
+++ importer/app.js
@@ -6,64 +6,40 @@ const { createBullBoard } = require('@bull-board/api');
 const { BullMQAdapter } = require('@bull-board/api/bullMQAdapter');
 const { ExpressAdapter } = require('@bull-board/express');
 const bodyParser = require('body-parser');
-const { worker } = require('./workers/apiWorker');
+const { workers } = require('./workers/apiWorker');
 
+require('dotenv').config()
 
 // Create Express app
 const app = express();
+
 // Use bodyparser to parse JSON requests
 app.use(bodyParser.json())
+
 // Allow CORS 
 app.use(cors({ origin: ['*'] }));
+
 // Create a Queue
-const jobsQ = new Queue('Imports', { connection : {
-  host: 'localhost',
-  port: 6379
+const jobsQ = new Queue(process.env.QUEUE_NAME, { connection : {
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT
 }})
+
 // Create an Adapter 
 const serverAdapter = new ExpressAdapter();
-serverAdapter.setBasePath('/admin'); 
+serverAdapter.setBasePath('/'); 
+
 // Configure UI Dashboard
 createBullBoard({
   queues: [new BullMQAdapter(jobsQ)],
   serverAdapter: serverAdapter,
 });
-// Configure routes for dashboard
-// this is where the dashboard will be shown.
-app.use('/admin', serverAdapter.getRouter());
 
-app.get('/', async (req, res) => {
-    const job = await jobsQ.add('Test', { orgid: '1', feedid: '2' }, {
-        repeat: {
-            cron: '44 * * * *',
-        },
-        removeOnComplete:{
-            count: 100
-        }
-    })
-    const job2 = await jobsQ.add('Test2', { orgid: '1', feedid: '2' }, {
-        repeat: {
-            cron: '44 * * * *',
-        },
-        removeOnComplete:{
-            count: 100
-        }
-    })
-    res.send({success: true, job1id: job.id, job2id: job2.id })
-})
+// Configure routes for dashboard
+app.use('/', serverAdapter.getRouter());
 
 // Start Server
 app.listen(3002, () => {
     console.log(`BullMQ Dashboard running on port 3002`);
 })
 
-
-// process.on("uncaughtException", function (err) {
-//     // Handle the error safely
-//     logger.error(err, "Uncaught exception");
-//   });
-  
-// process.on("unhandledRejection", (reason, promise) => {
-// // Handle the error safely
-//     logger.error({ promise, reason }, "Unhandled Rejection at: Promise");
-// });
