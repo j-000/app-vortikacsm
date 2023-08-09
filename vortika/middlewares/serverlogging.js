0983d064a2de711257c27bf1abb014321ec69dd5
Joao Oliveira <43117830+j-000@users.noreply.github.com>
2023-08-09 19:07:01 +0100

    more event listeners

--- vortika/middlewares/serverlogging.js
+++ vortika/middlewares/serverlogging.js
@@ -2,6 +2,10 @@ const chalk = require("chalk");
 
 function logrequests(req, res, next) {
     const { method, url } = req;
+    let userAgent = req.headers['user-agent'];
+    if (userAgent.length > 10) {
+      userAgent = userAgent.slice(0, 10);
+    }
     res.on('finish', () => {
       const { statusCode } = res;
       const statusColor =
@@ -10,9 +14,18 @@ function logrequests(req, res, next) {
           : statusCode >= 400
             ? chalk.yellow(`${statusCode} - ${method}`)
             : chalk.green(`${statusCode} - ${method}`);
-      const s = `${chalk.gray(new Date().toLocaleTimeString())} - ${statusColor} - ${chalk.blueBright(url)}`;
+      const s = `${chalk.gray(new Date().toLocaleTimeString())} - [${userAgent}] -${statusColor} - ${chalk.blueBright(url)}`;
       console.log(s);
-    });
+    })
+    req.on('data', () => {
+      console.log(`${chalk.red('DATA')}`)
+    })
+    req.on('aborter', () => {
+      console.log(`${chalk.red('ERROR')}`)
+    })
+    req.on('error', (err) => {
+      console.log(`${chalk.red('ERROR')} - ${err.message}`)
+    })
     next();
 } 
 
